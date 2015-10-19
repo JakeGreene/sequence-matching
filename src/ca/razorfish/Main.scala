09cfd21f7733a274d6e68131624dd055bba7e251
Jake Greene <jake.greene@razorfish.com>
2015-10-19 15:15:35 -0400

    Remove SNil

--- src/ca/razorfish/Main.scala
+++ src/ca/razorfish/Main.scala
@@ -2,19 +2,6 @@ package ca.razorfish
 
 import scala.collection.immutable.Queue
 
-final case object SNil extends Seq[Nothing] {
-  override def isEmpty: Boolean = true
-  override def length(): Int = 0
-  override def head: Nothing = throw new NoSuchElementException("head of empty sequence")
-  override def apply(index: Int): Nothing = throw new NoSuchElementException("accessing empty sequence")
-  override def tail: Seq[Nothing] = throw new UnsupportedOperationException("tail of empty sequence")
-  override def equals(that: Any) = that match {
-    case that1: scala.collection.GenSeq[_] => that1.isEmpty
-    case _ => false
-  }
-  override def iterator(): Iterator[Nothing] = Iterator()
-}
-
 object :+: {
   def unapply[A](s: Seq[A]): Option[(A, Seq[A])] = s match {
     case s if s.isEmpty => None
@@ -40,14 +27,20 @@ final case class :+:[A](override val head: A, private var tl: Seq[A]) extends Se
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val s = Queue(1, 2, 3)
-    //val s = "A" :+: "B" :+: SNil
     //val s = "A" :+: "B" :+: Queue("C")
     //val s = "A" :+: "B" :+: List("C")
     //val s = "A" :+: "B" :+: Vector("C")
+    
+    printHeadThenTail("A" :+: "B" :+: Nil) // Normal building but with a twist
+    printHeadThenTail("A" :+: Vector("B")) // Build with a sub-type of Seq
+    printHeadThenTail(Queue(1, 2, 3))
+  }
+  
+  def printHeadThenTail[T](s: Seq[T]): Unit = {
+    println(s"Matching $s with :+:")
     s match {
       case head :+: tail => println(s"$head then $tail")
-      case _ => println("Hmm")
+      case _ => println("Hmm. Nothing here")
     }
   }
 }
